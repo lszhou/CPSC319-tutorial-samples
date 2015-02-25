92ae6757a7508544b63eb62fee1552d61405515d
lszhou <lszhou2115@gmail.com>
2015-02-25 08:58:04 -0700

    updated。

--- src/T09/bufferReader.java
+++ src/T09/bufferReader.java
@@ -8,15 +8,13 @@ import java.io.IOException;
 public class bufferReader {
 
 	public static void main(String[] args) throws IOException {
-		
-		BufferedReader reader = null;
 
 		File file = new File("sample.txt"); //sample.txt is located at current workspace.
-		reader = new BufferedReader(new FileReader(file));
+		BufferedReader reader = new BufferedReader(new FileReader(file));
 
-		String line;
-		while ((line = reader.readLine()) != null) {
-			System.out.println(line);
+		String str;
+		while ((str = reader.readLine()) != null) {
+			System.out.println(str);
 		}
 
 		reader.close();
